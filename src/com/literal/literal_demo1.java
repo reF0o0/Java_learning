092e881b6ee6d0403fc174a22c66daf705d69ebc
“reF0o0” <1489076757@qq.com>
2026-08-13 16:47:30 +0800

    字面量

---
+++ src/com/literal/literal_demo1.java
@@ -0,0 +1,49 @@
+package com.literal;
+
+public class literal {
+    public static void main(String[] args) {
+        System.out.println("胡桃我爱你！");
+    }
+}
+
+// 注释（command + /）
+
+/*
+ * 多行注释（command + shift + /）
+ */
+
+// 字面量
+
+/*
+ * 字符串类型：
+ * "胡桃"
+ * "123" + "456" = "123456"
+ * 字符串拼接
+ */
+
+/*
+ * 字符类型：
+ * '女'
+ * 单引号，只能有一个数字、汉字
+ */
+
+/*
+ * 小数(double)类型：
+ * 13.14
+ */
+
+/*
+ * 整数(int)类型：
+ * 520
+ */
+
+/*
+ * 布尔(boll)类型：
+ * true
+ * false
+ */
+
+/*
+ * 空类型：
+ * null
+ */
