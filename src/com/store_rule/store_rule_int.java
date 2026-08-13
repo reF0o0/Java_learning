94b843a832c3365e72d3d53f02131785a771e3b9
“reF0o0” <1489076757@qq.com>
2026-08-13 17:28:53 +0800

    储存规则

---
+++ src/com/store_rule/store_rule_int.java
@@ -0,0 +1,14 @@
+package com.store_rule;
+
+public class store_rule_int {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/*
+ * 在计算机中，任意数据都是以二进制的形式来储存的
+ * 字节是计算机最小的储存单元，一个字节 = 8个bit位
+ * java中，int类型占四个字节/32个bit位
+ */
