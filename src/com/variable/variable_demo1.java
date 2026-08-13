2ca5b80a0b9e7c1fbd30bd71ff22abb70aa17cdb
“reF0o0” <1489076757@qq.com>
2026-08-13 17:17:12 +0800

    变量

---
+++ src/com/variable/variable_demo1.java
@@ -0,0 +1,17 @@
+package com.variable;
+
+public class variable_demo1 {
+    public static void main(String[] args) {
+        double wechat_money = 0;
+        double alipay_money = 10;
+        double bank_card_money = 20;
+        System.out.println(wechat_money + alipay_money + bank_card_money);
+        wechat_money = wechat_money + 10;
+        System.out.println(wechat_money);
+    }
+
+}
+/*
+ * 变量规范：
+ * 变量类型 变量名 = 数据值；
+ */
---
+++ src/com/variable/variable_demo3.java
@@ -0,0 +1,15 @@
+package com.variable;
+
+public class variable_demo3 {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        a = b = c = d = 1;
+    }
+}
+
+/*
+ * 只能存一个值
+ * 变量名不允许被重复定义
+ * 变量在使用前要赋值
+ * 一条语句可以连续赋值
+ */
