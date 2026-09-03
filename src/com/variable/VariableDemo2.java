29037e7894b2a993355bd5efbf6aa2fadfa18256
“reF0o0” <1489076757@qq.com>
2026-09-03 17:07:15 +0800

    变量

--- src/com/variable/VariableDemo2.java
+++ src/com/variable/VariableDemo2.java
@@ -3,14 +3,10 @@ package com.variable;
 public class VariableDemo2 {
     public static void main(String[] args) {
         int attack1 = 220;
-        int defense1 = 85;
-        double blood1 = 1012.5;
         double skill1 = 1.2;
 
-        int attack2 = 210;
         int defense2 = 80;
         double blood2 = 1223.3;
-        double skill2 = 1.3;
 
         double damage1 = attack1 - defense2;
         blood2 = blood2 - damage1;
--- src/com/variable/VariableDemo3.java
+++ src/com/variable/VariableDemo3.java
@@ -4,6 +4,11 @@ public class VariableDemo3 {
     public static void main(String[] args) {
         int a, b, c, d;
         a = b = c = d = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
 
